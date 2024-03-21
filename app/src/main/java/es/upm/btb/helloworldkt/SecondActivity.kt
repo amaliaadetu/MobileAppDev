7d9308d71070e8ea93bfa7ba1e432795082fe360
amaliaadetu <amaliaioana.adetu@alumnos.upm.es>
2024-03-21 02:59:35 +0100

    added the navbar, have to make it switch between activities

--- app/src/main/java/es/upm/btb/helloworldkt/SecondActivity.kt
+++ app/src/main/java/es/upm/btb/helloworldkt/SecondActivity.kt
@@ -7,9 +7,12 @@ import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import es.upm.btb.helloworldkt.databinding.ActivitySecondBinding
 
 
@@ -18,6 +21,7 @@ class SecondActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySecondBinding
     private lateinit var adapter: BarAdapter
     private lateinit var latestLocation: Location
+
 
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,11 +41,92 @@ class SecondActivity : AppCompatActivity() {
             BarManager.barList[position].isChecked = !BarManager.barList[position].isChecked
 
             if (BarManager.barList[position].isChecked) {
-                view.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.light_green))
+                view.setBackgroundColor(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.light_green
+                    )
+                )
             } else {
                 view.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
             }
 
         }
+
+
+        
+
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.bars -> {
+                    startActivity(Intent(this, SecondActivity::class.java))
+                    true
+                }
+                R.id.map -> {
+                    startActivity(Intent(this, OpenStreetMapActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
+
+//        binding.bottomNavigationView.setOnItemSelectedListener {
+//            when (it.itemId) {
+//                R.id.map -> startIntentMap()
+//
+//                else -> {
+//
+//                }
+//
+//            }
+//            true
+//        }
+
+//        val barNav: Button = findViewById(R.id.map)
+//        barNav.setOnClickListener {
+//            if (latestLocation != null) {
+//                val intent = Intent(this, OpenStreetMapActivity::class.java)
+//                val bundle = Bundle()
+//                bundle.putParcelable("location", latestLocation)
+//                intent.putExtra("locationBundle", bundle)
+//                startActivity(intent)
+//            }else{
+//                Log.e(TAG, "Location not set yet.")
+//            }
+//        }
+
+
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        menuInflater.inflate(R.menu.bottom_nav, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when (item.itemId) {
+//            R.id.bars -> {
+//                startActivity(Intent(this, SecondActivity::class.java))
+//                true
+//            }
+//            R.id.map -> {
+//                startActivity(Intent(this, OpenStreetMapActivity::class.java))
+//                true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+//        fun startIntentMap(): Boolean {
+//        val intent = Intent(this, OpenStreetMapActivity::class.java)
+//                val bundle = Bundle()
+//                bundle.putParcelable("location", latestLocation)
+//                intent.putExtra("locationBundle", bundle)
+//                startActivity(intent)
+//
+//            return true
+//        }
+
+
 }
